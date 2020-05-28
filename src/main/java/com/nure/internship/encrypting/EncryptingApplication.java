fb9188179185db01710a55947c63bc44cdd0e91a
Vladyslav Kaliuha <vladyslav_kaliuha@epam.com>
2020-05-28 17:45:59 +0300

    first commit

---
+++ src/main/java/com/nure/internship/encrypting/EncryptingApplication.java
@@ -0,0 +1,13 @@
+package com.nure.internship.encrypting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncryptingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncryptingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nure/internship/encrypting/EncryptingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nure.internship.encrypting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncryptingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
